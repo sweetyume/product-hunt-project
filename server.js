cc0a0809b0643cb134d3d15b688458bc2fec0541
sweetyume <sweet.tsukiyume@gmail.com>
2017-08-29 11:19:31 +0200

    add morgan, body-parser, pug, path, dossiers public, views  et stylesheets crées

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 const app = express();
+
+
+app.get('/', (request, response) => {
+  response.render('views/layout');
+});
 
 app.listen(3007, () => {
   console.log("Product Hunt arrive sur le port 3007");
