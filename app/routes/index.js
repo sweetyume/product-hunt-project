925e1d55614065523ea6d9b601ba5b088bc9cdd8
sweetyume <sweet.tsukiyume@gmail.com>
2017-08-29 23:49:13 +0200

    add edit and remove views and routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const Product = require('../models/product');
 
 // Get all products
+
 router.get('/', (request, response) => {
   Product.find((error, products) => {
     if (error) {
@@ -14,6 +15,7 @@ router.get('/', (request, response) => {
 });
 
 // Get a product information
+
 router.get('/products/:id', (request, response) => {
   Product.findById(request.params.id, (error, product) => {
     if (error) {
@@ -22,3 +24,49 @@ router.get('/products/:id', (request, response) => {
     response.render('product', { product });
   });
 });
+
+// Add a product
+
+router.get('/add', (request, response) => {
+  response.render('add_product');
+});
+
+router.post('/add', (request, response) => {
+  const product = new Product(request.body);
+  product.save((error) => {
+    if (error) {
+      response.send(error);
+    }
+    response.redirect('/');
+  });
+});
+
+// Edit a product
+router.get('/edit/:id', (request, response) => {
+  Product.findById(request.params.id, (error, product) => {
+    if (error) {
+      response.send(error);
+    }
+    response.render('edit_product', { product });
+  });
+});
+
+router.post('/edit/:id', (request, response) => {
+  Product.findByIdAndUpdate(request.params.id, request.body, (error) => {
+    if (error) {
+      response.send(error);
+    }
+    response.redirect('/');
+  });
+});
+
+// Delete a product// delete a product
+router.get('/remove/:id', (request, response) => {
+  Product.findByIdAndRemove(request.params.id, (error) => {
+    if (error) {
+      response.send(error);
+    }
+    response.redirect('/');
+  });
+});
+module.exports = router;
