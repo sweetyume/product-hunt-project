276ea82b54a22372e2d6d6ea100d10bee11ed022
sweetyume <sweet.tsukiyume@gmail.com>
2017-08-29 11:38:44 +0200

    add layout et home

--- server.js
+++ server.js
@@ -3,17 +3,20 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+const app = express();
+
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public'))); // express.static pour les dossiers statics = public
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-const app = express();
-
-
-app.get('/', (request, response) => {
-  response.render('views/layout');
+// ROUTES
+// ========================================
+app.get('/home', (request, response) => {
+  response.render('home');
 });
 
 app.listen(3007, () => {
